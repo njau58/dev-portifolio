84f6fd8e0c18158a0de0865347f5f92fca5c0450
njauprince@gmail.com <njauprince@gmail.com>
2023-03-15 21:36:49 +0300

    contact

--- pages/hello.ts
+++ pages/hello.ts
@@ -10,8 +10,5 @@ export default function handler(
   res: NextApiResponse<Data>
 ) {
 
-  res.statusCode = 200
-  res.setHeader('Content-Type', 'application/json')
-  res.end(JSON.stringify({ name: 'Robert' }))
-  
+
 }
