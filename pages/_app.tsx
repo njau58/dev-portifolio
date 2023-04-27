a30956af2441e7730fef7faa8f1a7e4cac517868
njauprince@gmail.com <njauprince@gmail.com>
2023-04-27 11:23:04 +0300

    vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,21 +1,16 @@
 import { useState, useEffect } from "react";
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-
+import { Analytics } from "@vercel/analytics/react";
 
 function MyApp({ Component, pageProps }: AppProps) {
- 
-
   return (
-   
-           <Component {...pageProps} />
-           
-
-          
-         
+    <>
+      {" "}
+      <Component {...pageProps} />
+      <Analytics></Analytics>
+    </>
   );
 }
 
 export default MyApp;
-  
-
